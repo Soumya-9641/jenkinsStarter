71082498a118b64b38e6965843c82f7ba81e8b49
Soumya-9641 <soumyadipgantait2003@gmail.com>
2025-03-17 20:49:36 +0530

    updating

--- server.ts
+++ server.ts
@@ -16,6 +16,9 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Routes
+app.get("/",(req,res)=>{
+    res.send("Hello jenkins server")
+})
 app.use("/api/tasks", taskRoutes);
 
 // Start server
